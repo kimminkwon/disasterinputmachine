d57073e3250d179260dc9e4898a5a19525eaffed
kimminkwon <kmk130519@gmail.com>
2020-10-16 15:05:01 +0900

    init commit
    201016

---
+++ src/main/java/org/koreanhistory/disasterinputmachine/DisasterinputmachineApplication.java
@@ -0,0 +1,13 @@
+package org.koreanhistory.disasterinputmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterinputmachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisasterinputmachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/koreanhistory/disasterinputmachine/DisasterinputmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreanhistory.disasterinputmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterinputmachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
