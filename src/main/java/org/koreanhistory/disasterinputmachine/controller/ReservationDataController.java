1d3e5b07450dc983de89da71c55421809d41462f
kimminkwon <kmk130519@gmail.com>
2020-10-26 20:40:34 +0900

    make modify job for ReservationData
    201026

--- src/main/java/org/koreanhistory/disasterinputmachine/controller/ReservationDataController.java
+++ src/main/java/org/koreanhistory/disasterinputmachine/controller/ReservationDataController.java
@@ -2,10 +2,7 @@ package org.koreanhistory.disasterinputmachine.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.koreanhistory.disasterinputmachine.dto.MaintenanceDataDto;
-import org.koreanhistory.disasterinputmachine.dto.MaintenanceDataSaveDto;
-import org.koreanhistory.disasterinputmachine.dto.ReservationDataDto;
-import org.koreanhistory.disasterinputmachine.dto.ReservationDataSaveDto;
+import org.koreanhistory.disasterinputmachine.dto.*;
 import org.koreanhistory.disasterinputmachine.service.ReservationDataService;
 import org.koreanhistory.disasterinputmachine.vo.PageMaker;
 import org.koreanhistory.disasterinputmachine.vo.PageVO;
@@ -64,5 +61,33 @@ public class ReservationDataController {
         rttr.addFlashAttribute("msg", "success");
 
         return "redirect:/reservation/list";
+    }
+
+    @GetMapping("/modify")
+    public void modifyGET(Long rno, @ModelAttribute("pageVO") PageVO vo, Model model) {
+        log.info("IN RESERVATION DATA CONTROLLER: modifyGET() called...");
+        log.info("RNO: " + rno);
+        ReservationDataDto dto = service.findById(rno);
+        log.info("FIND DTO: " + dto);
+        model.addAttribute("dto", dto);
+    }
+
+    // modifyGET()에서 데이터를 입력 후 Submit했을 경우
+    @PostMapping("/modify")
+    public String modifyPOST(@ModelAttribute("dto") ReservationDataModifyDto dto, PageVO vo, RedirectAttributes rttr) {
+        log.info("IN RESERVATION DATA CONTROLLER: modifyPOST() called...");
+        log.info("MODIFY DTO: " + dto);
+        service.modify(dto);
+
+        rttr.addFlashAttribute("msg", "success");
+        rttr.addAttribute("rno", dto.getRno());
+
+        // Paging과 검색 결과를 유지하기 위한 데이터 보내기
+        rttr.addAttribute("page", vo.getPage());
+        rttr.addAttribute("size", vo.getSize());
+        rttr.addAttribute("type", vo.getType());
+        rttr.addAttribute("keyword", vo.getKeyword());
+
+        return "redirect:/reservation/view";
     }
 }
--- src/main/java/org/koreanhistory/disasterinputmachine/service/ReservationDataService.java
+++ src/main/java/org/koreanhistory/disasterinputmachine/service/ReservationDataService.java
@@ -6,6 +6,7 @@ import org.koreanhistory.disasterinputmachine.domain.MaintenanceData;
 import org.koreanhistory.disasterinputmachine.domain.ReservationData;
 import org.koreanhistory.disasterinputmachine.dto.MaintenanceDataDto;
 import org.koreanhistory.disasterinputmachine.dto.ReservationDataDto;
+import org.koreanhistory.disasterinputmachine.dto.ReservationDataModifyDto;
 import org.koreanhistory.disasterinputmachine.dto.ReservationDataSaveDto;
 import org.koreanhistory.disasterinputmachine.repository.ReservationDataRepository;
 import org.koreanhistory.disasterinputmachine.vo.PageVO;
@@ -50,8 +51,24 @@ public class ReservationDataService {
         return new ReservationDataDto(entity);
     }
 
+    @Transactional
     public void save(ReservationDataSaveDto dto) {
         log.info("IN ReservationDataService: save() called...");
         repository.save(dto.toEntity());
+    }
+
+    @Transactional
+    public void modify(ReservationDataModifyDto dto) {
+        log.info("IN ReservationDataService: modify() called...");
+        log.info("DTO" + dto);
+        ReservationData entity = repository.findById(dto.getRno())
+                .orElseThrow(() -> new IllegalArgumentException("id가 존재하지 않습니다. id: " + dto.getRno()));
+        entity.update(dto);
+    }
+
+    @Transactional
+    public void deleteById(Long rno) {
+        log.info("IN ReservationDataService: deleteById() called...");
+        repository.deleteById(rno);
     }
 }
