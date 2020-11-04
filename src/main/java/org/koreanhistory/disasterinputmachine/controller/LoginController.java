6a1cca747e5b567f8fcce123badc0bbfdf4d379d
kimminkwon <kmk130519@gmail.com>
2020-11-04 14:57:42 +0900

    make maintenance list page to index page
    201103

--- src/main/java/org/koreanhistory/disasterinputmachine/controller/LoginController.java
+++ src/main/java/org/koreanhistory/disasterinputmachine/controller/LoginController.java
@@ -1,20 +1,26 @@
 package org.koreanhistory.disasterinputmachine.controller;
 
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Log
 public class LoginController {
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/maintenance/list";
+    }
     @GetMapping("/login")
     public void login() {
-
+        log.info("IN LOGIN CONTROLLER: calling login()...");
     }
     @GetMapping("/accessDenied")
     public void accessDenied() {
-
+        log.info("IN LOGIN CONTROLLER: calling accessDenied()...");
     }
     @GetMapping("/logout")
     public void logout() {
-
+        log.info("IN LOGIN CONTROLLER: calling logout()...");
     }
 }
