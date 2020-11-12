2783ffe6c892545bd0e0c546a60a49242c25c799
kimminkwon <kmk130519@gmail.com>
2020-11-12 15:01:38 +0900

    modify all views, SearchDto: add ltrtreClas(문헌 분류) column + Change Search from test function to all searching.
    201112

--- src/main/java/org/koreanhistory/disasterinputmachine/dto/SearchDto.java
+++ src/main/java/org/koreanhistory/disasterinputmachine/dto/SearchDto.java
@@ -40,7 +40,9 @@ public class SearchDto {
     // 2) 기사의 원문
     @Column(columnDefinition = "LONGTEXT")
     private String articlOrginl;
-    // 3) 기사 문헌의 명칭
+    // 3) 기사 문헌의 분류
+    private String ltrtreClas;
+    // 4) 기사 문헌의 명칭
     private String ltrtreNM;
 
     // 출전 (한글, 한자)
@@ -50,9 +52,9 @@ public class SearchDto {
     // 연도 (모호년)
     private String yearNameOfTomb;
     // 연도 (서기)
-    private int yearAD;
+    private String yearAD;
     // 연도 (월)
-    private int month;
+    private String month;
 
     // 국가, 왕조 (한국, 중국)
     private String dynastyKR;
@@ -77,19 +79,30 @@ public class SearchDto {
         String type = "";
 
         if(this.clasNo != "" && this.clasNo != null) type = strConcat(type, "clasNo");
-        if(this.indexKR != "" && this.indexKR != null) type = strConcat(type, "index");
-        if(this.lclasKR != "" && this.lclasKR != null) type = strConcat(type, "large");
-        if(this.mclasKR != "" && this.mclasKR != null) type = strConcat(type, "middle");
-        if(this.sclasKR != "" && this.sclasKR != null) type = strConcat(type, "small");
-        if(this.sourceKR != "" && this.sourceKR != null) type = strConcat(type, "source");
-        if(this.yearNameOfTomb != "" && this.yearNameOfTomb != null) type = strConcat(type, "yearTomb");
-        if(this.yearAD != 0) type = strConcat(type, "yearAD");
-        if(this.month != 0) type = strConcat(type, "month");
-        if(this.dynastyKR != "" && this.dynastyKR != null) type = strConcat(type, "dynasty");
-        if(this.area1KR != "" && this.area1KR != null) type = strConcat(type, "area1");
-        if(this.area2KR != "" && this.area2KR != null) type = strConcat(type, "area2");
-        if(this.area3KR != "" && this.area3KR != null) type = strConcat(type, "area3");
-        if(this.referIndex != "" && this.referIndex != null) type = strConcat(type, "refer");
+        if(this.indexKR != "" && this.indexKR != null) type = strConcat(type, "indexKR");
+        if(this.indexCN != "" && this.indexCN != null) type = strConcat(type, "indexCN");
+        if(this.lclasKR != "" && this.lclasKR != null) type = strConcat(type, "lclasKR");
+        if(this.lclasCN != "" && this.lclasCN != null) type = strConcat(type, "lclasCN");
+        if(this.mclasKR != "" && this.mclasKR != null) type = strConcat(type, "mclasKR");
+        if(this.mclasCN != "" && this.mclasCN != null) type = strConcat(type, "mclasCN");
+        if(this.sclasKR != "" && this.sclasKR != null) type = strConcat(type, "sclasKR");
+        if(this.sclasCN != "" && this.sclasCN != null) type = strConcat(type, "sclasCN");
+        if(this.ltrtreClas != "" && this.ltrtreClas != null) type = strConcat(type, "ltrtreClas");
+        if(this.ltrtreNM != "" && this.ltrtreNM != null) type = strConcat(type, "ltrtreNM");
+        if(this.sourceKR != "" && this.sourceKR != null) type = strConcat(type, "sourceKR");
+        if(this.sourceCN != "" && this.sourceCN != null) type = strConcat(type, "sourceCN");
+        if(this.yearNameOfTomb != "" && this.yearNameOfTomb != null) type = strConcat(type, "yearNameOfTomb");
+        if(this.yearAD != "" && this.yearAD != null) type = strConcat(type, "yearAD");
+        if(this.month != "" && this.month != null) type = strConcat(type, "month");
+        if(this.dynastyKR != "" && this.dynastyKR != null) type = strConcat(type, "dynastyKR");
+        if(this.dynastyCN != "" && this.dynastyCN != null) type = strConcat(type, "dynastyCN");
+        if(this.area1KR != "" && this.area1KR != null) type = strConcat(type, "area1KR");
+        if(this.area2KR != "" && this.area2KR != null) type = strConcat(type, "area2KR");
+        if(this.area3KR != "" && this.area3KR != null) type = strConcat(type, "area3KR");
+        if(this.area1CN != "" && this.area1CN != null) type = strConcat(type, "area1CN");
+        if(this.area2CN != "" && this.area2CN != null) type = strConcat(type, "area2CN");
+        if(this.area3CN != "" && this.area3CN != null) type = strConcat(type, "area3CN");
+        if(this.referIndex != "" && this.referIndex != null) type = strConcat(type, "referIndex");
         if(this.remark != "" && this.remark != null) type = strConcat(type, "remark");
 
         return type;
@@ -100,17 +113,28 @@ public class SearchDto {
 
         if(this.clasNo != "" && this.clasNo != null) keyword = strConcat(keyword, clasNo);
         if(this.indexKR != "" && this.indexKR != null) keyword = strConcat(keyword, indexKR);
+        if(this.indexCN != "" && this.indexCN != null) keyword = strConcat(keyword, indexCN);
         if(this.lclasKR != "" && this.lclasKR != null) keyword = strConcat(keyword, lclasKR);
+        if(this.lclasCN != "" && this.lclasCN != null) keyword = strConcat(keyword, lclasCN);
         if(this.mclasKR != "" && this.mclasKR != null) keyword = strConcat(keyword, mclasKR);
+        if(this.mclasCN != "" && this.mclasCN != null) keyword = strConcat(keyword, mclasCN);
         if(this.sclasKR != "" && this.sclasKR != null) keyword = strConcat(keyword, sclasKR);
+        if(this.sclasCN != "" && this.sclasCN != null) keyword = strConcat(keyword, sclasCN);
+        if(this.ltrtreClas != "" && this.ltrtreClas != null) keyword = strConcat(keyword, ltrtreClas);
+        if(this.ltrtreNM != "" && this.ltrtreNM != null) keyword = strConcat(keyword, ltrtreNM);
         if(this.sourceKR != "" && this.sourceKR != null) keyword = strConcat(keyword, sourceKR);
+        if(this.sourceCN != "" && this.sourceCN != null) keyword = strConcat(keyword, sourceCN);
         if(this.yearNameOfTomb != "" && this.yearNameOfTomb != null) strConcat(keyword, yearNameOfTomb);
-        if(this.yearAD != 0) keyword = strConcat(keyword, String.valueOf(yearAD));
-        if(this.month != 0) keyword = strConcat(keyword, String.valueOf(month));
+        if(this.yearAD != "" && this.yearAD != null) keyword = strConcat(keyword, yearAD);
+        if(this.month != "" && this.month != null) keyword = strConcat(keyword, month);
         if(this.dynastyKR != "" && this.dynastyKR != null) keyword = strConcat(keyword, dynastyKR);
+        if(this.dynastyCN != "" && this.dynastyCN != null) keyword = strConcat(keyword, dynastyCN);
         if(this.area1KR != "" && this.area1KR != null) keyword = strConcat(keyword, area1KR);
         if(this.area2KR != "" && this.area2KR != null) keyword = strConcat(keyword, area2KR);
         if(this.area3KR != "" && this.area3KR != null) keyword = strConcat(keyword, area3KR);
+        if(this.area1CN != "" && this.area1CN != null) keyword = strConcat(keyword, area1CN);
+        if(this.area2CN != "" && this.area2CN != null) keyword = strConcat(keyword, area2CN);
+        if(this.area3CN != "" && this.area3CN != null) keyword = strConcat(keyword, area3CN);
         if(this.referIndex != "" && this.referIndex != null) keyword = strConcat(keyword, referIndex);
         if(this.remark != "" && this.remark != null) keyword = strConcat(keyword, remark);
 
