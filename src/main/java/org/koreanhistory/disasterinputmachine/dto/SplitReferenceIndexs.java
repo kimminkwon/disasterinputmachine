7db8534a6ce20fb6d78cfad98f00ff84f43b56a3
kimminkwon <kmk130519@gmail.com>
2020-11-17 18:41:32 +0900

    error fix: modify split flag for example datas
    201117

--- src/main/java/org/koreanhistory/disasterinputmachine/dto/SplitReferenceIndexs.java
+++ src/main/java/org/koreanhistory/disasterinputmachine/dto/SplitReferenceIndexs.java
@@ -1,7 +1,19 @@
 package org.koreanhistory.disasterinputmachine.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class SplitReferenceIndexs {
     public String[] splitReferenceIndex(String refer) {
-        return refer.split(", ");
+        if(refer == null || refer.equals("")) return null;
+        List<String> splitedRefer = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(refer, "\\|");
+        while(st.hasMoreTokens())
+            splitedRefer.add(st.nextToken());
+
+        String[] refers = splitedRefer.toArray(new String[0]);
+        return refers;
     }
 }
