b3b5f3b0fd7c4e0f2b01ee185516ae4b07460b61
kimminkwon <kmk130519@gmail.com>
2020-12-14 21:25:40 +0900

    error fix: renaming method
    201214

--- src/test/java/org/koreanhistory/disasterinputmachine/mapping/ClasMappingTest.java
+++ src/test/java/org/koreanhistory/disasterinputmachine/mapping/ClasMappingTest.java
@@ -13,25 +13,25 @@ public class ClasMappingTest {
 
         assertThat(instance).isEqualTo(instance2);
 
-        assertThat(instance.getAreaDatas("10101")[0]).isEqualTo("1 천변");
-        assertThat(instance.getAreaDatas("10101")[1]).isEqualTo("1 天變");
-        assertThat(instance.getAreaDatas("10101")[2]).isEqualTo("01 천변");
-        assertThat(instance.getAreaDatas("10101")[3]).isEqualTo("01 天變");
-        assertThat(instance.getAreaDatas("10101")[4]).isEqualTo("01천변");
-        assertThat(instance.getAreaDatas("10101")[5]).isEqualTo("01天變");
+        assertThat(instance.getClasDatas("10101")[0]).isEqualTo("1 천변");
+        assertThat(instance.getClasDatas("10101")[1]).isEqualTo("1 天變");
+        assertThat(instance.getClasDatas("10101")[2]).isEqualTo("01 천변");
+        assertThat(instance.getClasDatas("10101")[3]).isEqualTo("01 天變");
+        assertThat(instance.getClasDatas("10101")[4]).isEqualTo("01천변");
+        assertThat(instance.getClasDatas("10101")[5]).isEqualTo("01天變");
 
-        assertThat(instance.getAreaDatas("30401")[0]).isEqualTo("3 변괴");
-        assertThat(instance.getAreaDatas("30401")[1]).isEqualTo("3 變怪");
-        assertThat(instance.getAreaDatas("30401")[2]).isEqualTo("04 이현");
-        assertThat(instance.getAreaDatas("30401")[3]).isEqualTo("04 異現");
-        assertThat(instance.getAreaDatas("30401")[4]).isEqualTo("01이상현상");
-        assertThat(instance.getAreaDatas("30401")[5]).isEqualTo("01이상현상");
+        assertThat(instance.getClasDatas("30401")[0]).isEqualTo("3 변괴");
+        assertThat(instance.getClasDatas("30401")[1]).isEqualTo("3 變怪");
+        assertThat(instance.getClasDatas("30401")[2]).isEqualTo("04 이현");
+        assertThat(instance.getClasDatas("30401")[3]).isEqualTo("04 異現");
+        assertThat(instance.getClasDatas("30401")[4]).isEqualTo("01이상현상");
+        assertThat(instance.getClasDatas("30401")[5]).isEqualTo("01이상현상");
 
-        assertThat(instance.getAreaDatas("60101")[0]).isEqualTo("6 기타");
-        assertThat(instance.getAreaDatas("60101")[1]).isEqualTo("6 기타");
-        assertThat(instance.getAreaDatas("60101")[2]).isEqualTo("01 기타");
-        assertThat(instance.getAreaDatas("60101")[3]).isEqualTo("01 기타");
-        assertThat(instance.getAreaDatas("60101")[4]).isEqualTo("01기타");
-        assertThat(instance.getAreaDatas("60101")[5]).isEqualTo("01기타");
+        assertThat(instance.getClasDatas("60101")[0]).isEqualTo("6 기타");
+        assertThat(instance.getClasDatas("60101")[1]).isEqualTo("6 기타");
+        assertThat(instance.getClasDatas("60101")[2]).isEqualTo("01 기타");
+        assertThat(instance.getClasDatas("60101")[3]).isEqualTo("01 기타");
+        assertThat(instance.getClasDatas("60101")[4]).isEqualTo("01기타");
+        assertThat(instance.getClasDatas("60101")[5]).isEqualTo("01기타");
     }
 }
