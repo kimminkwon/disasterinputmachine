77944d62e30201bc7b7a4479549ec5cca7bdb7f6
kimminkwon <kmk130519@gmail.com>
2020-12-25 20:25:15 +0900

    modify dto: add mapping years in mapping method
    201225

--- src/main/java/org/koreanhistory/disasterinputmachine/dto/ReservationDataModifyDto.java
+++ src/main/java/org/koreanhistory/disasterinputmachine/dto/ReservationDataModifyDto.java
@@ -2,9 +2,7 @@ package org.koreanhistory.disasterinputmachine.dto;
 
 import lombok.*;
 import org.koreanhistory.disasterinputmachine.domain.ReservationData;
-import org.koreanhistory.disasterinputmachine.mapping.AreaInGoryeoMapping;
-import org.koreanhistory.disasterinputmachine.mapping.AreaInJosunMapping;
-import org.koreanhistory.disasterinputmachine.mapping.ClasMapping;
+import org.koreanhistory.disasterinputmachine.mapping.*;
 
 import javax.persistence.Column;
 
@@ -98,12 +96,26 @@ public class ReservationDataModifyDto {
         String[] clasDatas = ClasMapping.getInstance().getClasDatas(clasNo);
         this.lclasKR = clasDatas[0]; this.lclasCN = clasDatas[1]; this.mclasKR = clasDatas[2]; this.mclasCN = clasDatas[3]; this.sclasKR = clasDatas[4]; this.sclasCN = clasDatas[5];
 
-        if(dynastyKR != null && dynastyKR.equals("조선")) {
-            this.area1CN = area1KR.equals("") || area1KR == null ? "" : AreaInJosunMapping.getInstance().getAreaOfChina(area1KR);
-            this.area2CN = area2KR.equals("") || area2KR == null ? "" : AreaInJosunMapping.getInstance().getAreaOfChina(area2KR);
-        } else if(dynastyKR != null && dynastyKR.equals("고려")) {
-            this.area1CN = area1KR.equals("") || area1KR == null ? "" : AreaInGoryeoMapping.getInstance().getAreaOfChina(area1KR);
-            this.area2CN = area2KR.equals("") || area2KR == null ? "" : AreaInGoryeoMapping.getInstance().getAreaOfChina(area2KR);
+        if(dynastyKR != null) {
+            if(dynastyKR.equals("조선") || dynastyKR.equals("朝鮮") || dynastyKR.equals("조선(朝鮮)")) {
+                this.area1CN = area1KR.equals("") || area1KR == null ? "" : AreaInJosunMapping.getInstance().getAreaOfChina(area1KR);
+                this.area2CN = area2KR.equals("") || area2KR == null ? "" : AreaInJosunMapping.getInstance().getAreaOfChina(area2KR);
+            } else if(dynastyKR.equals("고려") || dynastyKR.equals("高麗") || dynastyKR.equals("고려(高麗)")) {
+                this.area1CN = area1KR.equals("") || area1KR == null ? "" : AreaInGoryeoMapping.getInstance().getAreaOfChina(area1KR);
+                this.area2CN = area2KR.equals("") || area2KR == null ? "" : AreaInGoryeoMapping.getInstance().getAreaOfChina(area2KR);
+            }
+        }
+
+        if(dynastyCN == null || dynastyCN.equals("") || dynastyCN.equals(" ")) {
+            if(dynastyKR != null) {
+                this.yearAD = DynastyKRMapping.getInstance().getYearADAndAge(dynastyKR, yearNameOfTomb)[0];
+                this.yearAge = DynastyKRMapping.getInstance().getYearADAndAge(dynastyKR, yearNameOfTomb)[1];
+            }
+        } else if(dynastyKR == null || dynastyKR.equals("") || dynastyKR.equals(" ")) {
+            if(dynastyCN != null) {
+                this.yearAD = DynastyCNMapping.getInstance().getYearADAndNameOfTomb(dynastyCN, yearAge)[0];
+                this.yearNameOfTomb = DynastyCNMapping.getInstance().getYearADAndNameOfTomb(dynastyCN, yearAge)[1];
+            }
         }
     }
 }
